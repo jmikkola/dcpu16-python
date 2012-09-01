c2aae58222f3cfea38f3d3fd4ec42deb772f66c0
Jeremy Mikkola <jeremy128@gmail.com>
2012-09-01 13:03:52 -0700

    Finished crude parser

--- dcpu16.py
+++ dcpu16.py
@@ -5,6 +5,7 @@ import re
 import collections
 
 Value = collections.namedtuple('Value', ['kind', 'indirect', 'value'])
+Instruction = collections.namedtuple('Instruction', ['opcode', 'num_values'])
 
 class ParseError(Exception):
     def __init__(self, value):
@@ -36,18 +37,42 @@ def read_value(token):
         return Value('Literal', indirect, int(token, 16))
     raise ParseError('Expected a value, found ' + token)
 
-def read_instruction(line):
-    return None
+def strip_comment(line):
+    index = line.find(';')
+    return line[:index] if index >= 0 else line
+
+basicOpcodes = set('SET ADD MUL DIV MOD SHL SHR AND BOR XOR IFE IFN IFG IFB'.split())
+def read_instruction(token):
+    assert(token)
+    if token == 'JSR':
+        return Instruction(token, 1)
+    elif token in basicOpcodes:
+        return Instruction(token, 2)
+    else:
+        raise ParseError('Bad opcode ' + token);
 
 def parse_line(line):
-    parts = line.strip().split()
-    if parts and parts[0][0] == ':':
-        yield read_label(parts[0])
-        parts = parts[1:]
-    for part in parts:
-        if part[0] == ';':
-            break
-        yield part
+    tokens = strip_comment(line).strip().split()
+    state = 1
+    for token in tokens:
+        if state == 1 and token[0] == ':':
+            state = 2
+            yield read_label(token)
+        elif state in [1,2]:
+            instruction = read_instruction(token)
+            state = 5 - instruction.num_values
+            yield instruction
+        elif state in [3,4]:
+            state += 1
+            yield read_value(token)
+        else:
+            raise ParseError('Did not expect %s in state %d' % (token, state))
+    if state not in [1,2,5]:
+        raise ParseError('Line ended before parse finished. Final state: %d' % state)
+
+def echo_line(line):
+    for item in parse_line(line):
+        print item
 
 def parse(inf):
     ''' A simple line-based parser '''
