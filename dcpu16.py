9066f61c6cdb0a7ddd9358fab2b5344292e26792
Jeremy Mikkola <jeremy128@gmail.com>
2012-09-01 22:53:13 -0700

    Rework parsing and assembly structure

--- dcpu16.py
+++ dcpu16.py
@@ -4,57 +4,170 @@ import sys
 import re
 
 class ParseItem:
-    def is_value():
+    def is_instruction(self):
         return False
-    def is_instruction():
-        return False
-    def is_label():
-        return False
-    def output(labels):
-        return []
 
-class Value(ParseItem):
-    def __init__(self, kind, indirect, value, plus=None):
-        self.kind = kind
-        self.value = value
-        self.indirect = indirect
-        self.plus = plus
-    def is_value():
-        return True
-    def __str__(self):
-        s = ['['] if self.indirect else []
-        s.append(str(self.value))
-        if self.plus: s.extend(['+', self.plus])
-        if self.indirect: s.append(']')
-        return ''.join(s)
-    def __repr__(self):
-        return "Value(%s, %s, %s, %s)" % (
-            repr(self.kind), repr(self.indirect), repr(self.value), repr(self.plus))
+    def is_label(self):
+        return False
 
 class Label(ParseItem):
     def __init__(self, label):
         self.label = label
-    def is_label():
+
+    def is_label(self):
         return True
+
     def __str__(self):
         return ':' + self.label
+
     def __repr__(self):
         return 'Label(%s)' % repr(self.label)
 
+class Value:
+    commands = {
+        'POP':  0x18, 'PEEK': 0x19, 'PUSH': 0x1A,
+        'SP':   0x1B, 'PC':   0x1C, 'O':    0x1D
+    }
+    registers = {
+        'A': 0, 'B': 1, 'C': 2,
+        'X': 3, 'Y': 4, 'Z': 5,
+        'I': 6, 'J': 7
+    }
+    literalRe = re.compile(r'^\d+$')
+    literalRe16 = re.compile(r'^0x[0-9a-fA-F]{1,4}$')
+    labelRe = re.compile(r'^\w+$')
+
+    @classmethod
+    def parse(cls, token):
+        assert(token)
+
+        # Is access indirect?
+        indirect = False
+        if token[0] == '[' and token[-1] == ']':
+            token = token[1:-1]
+            indirect = True
+
+        # Values that cannot have a register offset
+        if token in cls.commands:
+            return cls(cls.commands[token], indirect)
+        if len(token) == 1 and token in cls.registers:
+            r = cls.registers[token]
+            if indirect: r += 8
+            return cls(r, indirect)
+
+        # Does it have a register offset?
+        offset = None
+        if indirect and (len(token) > 1 and token[-2] == '+'):
+            offset = token[-1]
+            if offset not in cls.registers:
+                raise ParseError('Bad register offset: ' + offset)
+            r = cls.registers[offset]
+            token = token[:-2]
+
+        # Values that can have a register offset
+        if cls.literalRe.match(token):
+            literal = int(token)
+            if not indirect and 0 <= literal < 0x20:
+                return cls(literal + 0x20)
+            if offset:
+                value = 0x10 + r
+            elif indirect:
+                value = 0x1e
+            else:
+                value = 0x1f
+            return cls(value, hasNextWord=True, nextWord=literal)
+        if cls.literalRe16.match(token):
+            literal = int(token, 16)
+            if not indirect and 0 <= literal < 0x20:
+                return cls(literal + 0x20)
+            if offset:
+                value = 0x10 + r
+            elif indirect:
+                value = 0x1e
+            else:
+                value = 0x1f
+            return cls(value, hasNextWord=True, nextWord=literal)
+            return cls('Literal', indirect, int(token, 16), plus)
+        if cls.labelRe.match(token):
+            value = 0x1e if indirect else 0x1f
+            return cls(value, hasNextWord=True, label=token)
+        raise ParseError('Expected a value, found ' + token)
+
+
+    def __init__(self, value, hasNextWord = False, nextWord=None, label=None):
+        self.value = value
+        self.nextWord = nextWord
+        self.hasNextWord = hasNextWord
+        self.label = label
+
+    def __str__(self):
+        # TODO: un-parse value
+        return repr(self)
+
+    def __repr__(self):
+        return "Value(%s, %s)" % (repr(self.value), repr(self.nextValue))
+
+    def requires_lookup(self):
+        return self.hasNextWord and self.label is not None
+
 class Instruction(ParseItem):
-    def __init__(self, opcode, num_values):
+    values = {
+        'SET': 0x01, 'ADD': 0x02, 'SUB': 0x03, 'MUL': 0x04, 'DIV': 0x05, 'MOD': 0x06,
+        'SHL': 0x07, 'SHR': 0x08, 'AND': 0x09, 'BOR': 0x0A, 'XOR': 0x0B, 'IFE': 0x0C,
+        'IFN': 0x0D, 'IFG': 0x0E, 'IFB': 0x0F, 'JSR': 0x10
+    }
+
+    @classmethod
+    def parse(cls, tokens):
+        if not (2 <= len(tokens) <= 3):
+            raise ParseError('Expected 2 or 3 part instruction: ' + str(tokens))
+
+        opcode = tokens[0]
+        if opcode not in cls.values:
+            raise ParseError('Invalid opcode: ' + opcode)
+        instruction = cls(cls.values[opcode])
+
+        values = tokens[1:]
+        if len(values) != instruction.num_values():
+            raise ParseError('Wrong number of values for ' + str(instruction))
+        if len(values) == 1:
+            instruction.values.append(Value.parse(values[0]))
+        else:
+            # TODO: check for comma
+            instruction.values.append(Value.parse(values[0][:-1]))
+            instruction.values.append(Value.parse(values[1]))
+        return instruction
+
+    def __init__(self, opcode):
         self.opcode = opcode
-        self.num_values = num_values
-    def is_instruction():
+        self.values = []
+
+    def is_instruction(self):
         return True
+
+    def num_values(self):
+        return 2 if (self.opcode != 0x10) else 1
+
     def __str__(self):
         return self.opcode
+
     def __repr__(self):
         return 'Instruction(%s, %d)' % (repr(self.opcode), self.num_values)
+
+    def output(self):
+        out = [self.opcode]
+        for v in self.values:
+            out.append(v.value)
+        for v in self.values:
+            if v.hasNextWord:
+                out.append(v.nextWord)
+        return out
+
 
 class ParseError(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
@@ -65,79 +178,19 @@ def read_label(token):
         raise ParseError('Label must be of format :label, not ' + token)
     return Label(token[1:])
 
-literalRe = re.compile(r'^\d+$')
-literalRe16 = re.compile(r'^0x[0-9a-fA-F]{1,4}$')
-labelRe = re.compile(r'^\w+$')
-commands = {'POP', 'PEEK', 'PUSH', 'SP', 'PC', 'O'}
-registers = 'ABCXYZIJ'
-def read_value(token):
-    ''' parse a value '''
-    assert(token)
-
-    # Is access indirect?
-    indirect = False
-    if token[0] == '[' and token[-1] == ']':
-        token = token[1:-1]
-        indirect = True
-
-    # Values that cannot have a register offset
-    if token in commands:
-        return Value('Command', indirect, token)
-    if len(token) == 1 and token in registers:
-        return Value('Register', indirect, token)
-
-    # Does it have a register offset?
-    plus = None
-    if indirect and (len(token) > 1 and token[-2] == '+'):
-        plus = token[-1]
-        if plus not in registers:
-            raise ParseError('Bad register offset: ' + plus)
-        token = token[:-2]
-
-    # Values that can have a register offset
-    if literalRe.match(token):
-        return Value('Literal', indirect, int(token), plus)
-    if literalRe16.match(token):
-        return Value('Literal', indirect, int(token, 16), plus)
-    if labelRe.match(token):
-        return Value('label', indirect, token, plus)
-    raise ParseError('Expected a value, found ' + token)
 
 def strip_comment(line):
     index = line.find(';')
     return line[:index] if index >= 0 else line
 
-basicOpcodes = set('SET ADD SUB MUL DIV MOD SHL SHR AND BOR XOR IFE IFN IFG IFB'.split())
-def read_instruction(token):
-    assert(token)
-    if token == 'JSR':
-        return Instruction(token, 1)
-    elif token in basicOpcodes:
-        return Instruction(token, 2)
-    else:
-        raise ParseError('Bad opcode ' + token);
-
 def parse_line(line):
     tokens = strip_comment(line).strip().split()
-    state = 1
-    for token in tokens:
-        if state == 1 and token[0] == ':':
-            state = 2
-            yield read_label(token)
-        elif state in [1,2]:
-            instruction = read_instruction(token)
-            state = 5 - instruction.num_values
-            yield instruction
-        elif state in [3,4]:
-            if state == 3:
-                if token[-1] == ',': token = token[:-1]
-                else: raise ParseError("Expected value to end with a ',': " + token)
-            state += 1
-            yield read_value(token)
-        else:
-            raise ParseError('Did not expect %s in state %d' % (token, state))
-    if state not in [1,2,5]:
-        raise ParseError('Line ended before parse finished. Final state: %d' % state)
+    while tokens and tokens[0][0] == ':':
+        # TODO: convert to Label.parse
+        yield read_label(tokens[0])
+        tokens = tokens[1:]
+    if tokens:
+        yield Instruction.parse(tokens)
 
 def parse(inf):
     ''' A simple line-based parser '''
@@ -146,17 +199,35 @@ def parse(inf):
             yield item
 
 def assemble(items):
+    data = []
+    labels = {}
+    updateLocations = {}
+
+    wordPosition = 0
     for x in items:
-        print x
+        if x.is_instruction():
+            data.extend(x.output())
+            # TODO: note locations of labels
+        else:
+            label = x.label
+            if label in labels:
+                raise ParseError("duplicate label: " + label)
+            labels[label] = len(data)
+
+    for location, label in updateLocations.iteritems():
+        if label not in labels:
+            raise ParseError("could not find label: " + label)
+        labelLocation = labels[label]
+        date[labelLocation] = location
+    return data
 
 def main(args):
     if args:
         fname = args[0]
         with open(fname, 'r') as inf:
-            assemble(parse(inf))
+            print assemble(parse(inf))
     else:
-        assemble(parse(sys.stdin))
+        print assemble(parse(sys.stdin))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-    #pass
